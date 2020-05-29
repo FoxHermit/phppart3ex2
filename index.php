e824f32b3a379a72b233cc1ff10e8800c8f6e9d5
FoxHermit <62595041+FoxHermit@users.noreply.github.com>
2020-05-29 18:47:47 +0200

    corrigé

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+$months = array();
+
+$months[0] = 'Janvier';
+$months[1] = 'Février';
+$months[2] = 'Mars';
+$months[3] = 'Avril';
+$months[4] = 'Mai';
+$months[5] = 'Juin';
+$months[6] = 'Juillet';
+$months[7] = 'Aout';
+$months[8] = 'Septembre';
+$months[9] = 'Octobre';
+$months[10] = 'Novembre';
+$months[11] = 'Décembre';
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Exercice 2</title>
+  </head>
+  <body>
+<p><?php echo $months[0]; ?></p>
+<p><?php echo $months[1]; ?></p>
+<p><?php echo $months[2]; ?></p>
+<p><?php echo $months[3]; ?></p>
+<p><?php echo $months[4]; ?></p>
+<p><?php echo $months[5]; ?></p>
+<p><?php echo $months[6]; ?></p>
+<p><?php echo $months[7]; ?></p>
+<p><?php echo $months[8]; ?></p>
+<p><?php echo $months[9]; ?></p>
+<p><?php echo $months[10]; ?></p>
+<p><?php echo $months[11]; ?></p>
+  </body>
+</html>
